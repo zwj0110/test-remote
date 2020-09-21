00e3fd37b3899068e3abfbd4a39eb63280cbbc7c
zhengwenjie <380399972@qq.com>
2020-09-21 23:17:12 +0800

    hello world

--- src/URLUtils.java
+++ src/URLUtils.java
@@ -8,7 +8,7 @@ public class URLUtils {
         String url = "";
 
         for (Map.Entry<String, String> stringStringEntry : entrySet) {
-            System.out.println();
+            System.out.println("hello world");
         }
         return url;
     }
